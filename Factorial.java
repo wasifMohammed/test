af691d577c0fb9dddcb7a3a2045beca9fd9fa088
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-07-17 16:50:34 +0530

    initial
    init factorial

---
+++ Factorial.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorial;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author DELL
+ */
+public class Factorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int n,result,i=0;
+       Scanner read = new Scanner(System.in);
+       n=read.nextInt();
+       result=n;
+       if(n<=20){
+           for(i=1;i<n;++i){
+           result=result*(n-i);
+           }
+           }
+       System.out.println(result);// TODO code application logic here
+    }
+    
+}
