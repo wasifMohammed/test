aae904f394618df32f3e69c3835f415df1266cbf
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-07-19 13:49:11 +0530

    appending dot
    Appending a dot at the end of the string.

---
+++ appenddot.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+
+    string str;
+
+    
+getline(cin,str);
+
+    
+//cout<<str<<endl;
+
+    str.push_back('.');
+
+    cout<<str<<endl;
+
+    return 0;
+    
+
+}
