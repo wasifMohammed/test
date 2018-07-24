90c780834853521a4bae9d37e1e3942a088fa819
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-07-24 10:26:47 +0530

    if same letters and unable to swap, print no

---
+++ JavaApplication20.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication20;
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author DELL
+ */
+public class JavaApplication20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i,flag=0;
+       String str0=  new String();
+       Scanner read=new Scanner(System.in);
+       System.out.println("Enter the String ..");
+       str0=read.nextLine();
+       char[] chars=new char[20];
+       char[] temp=new char[20];
+       chars=str0.toCharArray();
+       for(i=0;i<str0.length()-1;++i){
+           if(chars[i]==chars[i+1]){
+           System.out.println("NO..");
+           flag=1;
+           }
+           else if(chars[i]!=chars[i+1])
+           {
+           temp[i]=chars[i];
+           chars[i]=chars[i+1];
+           chars[i+1]=temp[i];
+           
+           }
+       
+       }
+       
+       if(flag!=1){
+           System.out.println("YES..");
+           for(i=0;i<str0.length();++i){
+           //System.out.println(chars[i]);
+           }
+       
+       }
+       
+               
+
+    }
+    
+}
