cf26bcf7334b9778ad2caa06518af95c264d8350
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-07-31 10:51:04 +0530

    Update PartionEqualGroups.py

--- PartionEqualGroups.py
+++ PartionEqualGroups.py
@@ -5,18 +5,20 @@
 	i=0
 	c1=0
 	c2=0
-	while num>0:
-		num=num-a
-		c1+=1
-		if(num!=0):
-			num=num-b
-			c2+=1
-		else:
-			print "no"
-	if(num==0 and c2==c1):
-		print "yes"
-	else:
-		print "no"
+	if num<=100000:
+		if a<=100000 and b<=100000:
+			while num>0:
+				num=num-a
+				c1+=1
+				if(num!=0):
+					num=num-b
+					c2+=1
+				else:
+					print "no"
+			if(num==0 and c2==c1):
+				print "yes"
+			else:
+				print "no"
 
 		
 >>> main()
