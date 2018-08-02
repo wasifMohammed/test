18686b68f2f0780ab3df8b7de6e281214dbdd3ac
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-08-02 10:55:30 +0530

    Update Capitalizefirst.java

--- Capitalizefirst.java
+++ Capitalizefirst.java
@@ -15,7 +15,8 @@ public class Capitalizefirst {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int i=0,j,flag=0;
         String str0=new String();
         Scanner read=new Scanner(System.in);
@@ -24,37 +25,35 @@ public class Capitalizefirst {
         String newstr=new String();
         newstr="";
         char[] chars=new char[20];
+       
         chars=str0.toCharArray();
+        
         if(str0.length()<=100000){
-        for(i=0;i<str0.length();++i){
-          
-            if (i>=0&&i<str0.length()){
-             if(chars[i]!=' '){
-                  if(i%2==0){
-                      newstr=newstr+Character.toUpperCase(chars[i]);
-                  }else if(i%2!=0){
-                      newstr=newstr+Character.toLowerCase(chars[i]);
-                  }
-              }else if(chars[i]==' '){
-                  
-                 //i++;
-                  //newstr=newstr+' ';
-                  //i++;
-                  if(i%2!=0){
-                      newstr=newstr+Character.toUpperCase(chars[i]);
-                  }else if(i%2==0){
-                      newstr=newstr+Character.toLowerCase(chars[i]);
-                  }
-                  
-                  i--;
-                  i++;
-               } 
+            for(i=0;i<str0.length();++i){
+                if (i>=0&&i<str0.length()){
+                    if(chars[i]!=' '){
+                        if(i%2==0){
+                            newstr=newstr+Character.toUpperCase(chars[i]);
+                        }else if(i%2!=0){
+                            newstr=newstr+Character.toLowerCase(chars[i]);
+                        }
+                    }else if(chars[i]==' '){
+                        //i++;
+                        //newstr=newstr+' ';
+                        //i++;
+                        if(i%2!=0){
+                            newstr=newstr+Character.toUpperCase(chars[i]);
+                        }else if(i%2==0){
+                            newstr=newstr+Character.toLowerCase(chars[i]);
+                        }
+                        i--;
+                        i++;
+                    } 
                }
-          
             }
-          }
-        System.out.println(newstr);
         }
-      }
+        System.out.println(newstr);
+    }
+}
     
 
