0ad0c1a743e72f4fa2a90d17b2e035a49784ba9d
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-08-02 11:11:07 +0530

    Update Commonprefix.java

--- Commonprefix.java
+++ Commonprefix.java
@@ -14,52 +14,50 @@ public class Commonprefix {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int n,i,j,len0,len1;
         //String str0=new String();
-        StringBuilder strb=new StringBuilder();
+        StringBuilder strb = new StringBuilder();
         System.out.println("Enter the number of strings..");
-        Scanner read=new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
         n=read.nextInt();
-        String[] str1=new String[n];
-        char[] temp0=new char[20];
-        char[] temp1=new char[20];
-        char[] result=new char[20];
+        String[] str1 = new String[n];
+        char[] temp0  = new char[20];
+        char[] temp1  = new char[20];
+        char[] result = new char[20];
         for(i=0;i<n;++i){
-            str1[i]=read.next();  
+            str1[i] = read.next();  
             }
-        for(j=0;j<n-1;++j){
-            temp0=str1[j].toCharArray();
-            temp1=str1[j+1].toCharArray();
-            len0=temp0.length;
-            len1=temp1.length;
+        for(j = 0;j < n-1; ++j){
+            temp0 = str1[j].toCharArray();
+            temp1 = str1[j+1].toCharArray();
+            len0  = temp0.length;
+            len1  = temp1.length;
             if(len0<len1){
-                for(i=0;i<len0;++i){
-                    if(temp0[i]==temp1[i]){
-                        result[i]=temp0[i];
+                for(i = 0; i < len0; ++i){
+                    if(temp0[i] == temp1[i]){
+                        result[i] = temp0[i];
                     } 
                 }
                 //System.out.print(result);
-            }else if(len0>len1){
-                for(i=0;i<len1;++i){
-                    if(temp0[i]==temp1[i]){
-                        result[i]=temp0[i];
+            }else if(len0 > len1){
+                for(i = 0;i < len1; ++i){
+                    if(temp0[i] == temp1[i]){
+                        result[i] = temp0[i];
                     }
                    // System.out.print(result);
-            }
-              
-                    
-            //System.out.print(str1[i]+" ");  
+                }
+                //System.out.print(str1[i]+" ");  
             }else {
-                    for(i=0;i<len1;++i){
-                    if(temp0[i]==temp1[i]){
-                        result[i]=temp0[i];
+                for(i = 0; i < len1; ++i){
+                    if(temp0[i] == temp1[i]){
+                        result[i] = temp0[i];
                     }
+                }
+                //System.out.print(result);
             }
-                    //System.out.print(result);
-            }
-    }
+        }
         System.out.print(result);
-    
-}
+    }
 }
