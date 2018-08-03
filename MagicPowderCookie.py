c305a0097fcb98094123364e930c705ef79e44b5
wasifMohammed <41318215+wasifMohammed@users.noreply.github.com>
2018-08-03 11:27:54 +0530

    Update MagicPowderCookie.py

--- MagicPowderCookie.py
+++ MagicPowderCookie.py
@@ -12,23 +12,13 @@
 		re.append(input("Enter the required quantity in grams.. "))
 	for i in range(n):
 		av.append(input("Enter the available quantity in grams.. "))
-	print "n = ",n
-	print re
-	print av
 	for i in range(n):
 		if(av[i]>0 and re[i]>0):
 			if(av[i]>=re[i]):
 				c.append(av[i]/re[i])
 				av[i]%=re[i]
-				print av[i]
-	print c
-	print re
-	print av
 	minin=c.index(min(c))
 	ch=av[minin]%re[minin]
-	print minin
-	print "av[minin]", av[minin]
-	print "re[minin] ",re[minin]
 	if(ch<=re[minin]):
 		c[minin]+=k
 		#c[minin]=c[minin]/re[minin]
